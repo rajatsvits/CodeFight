a087bc63def6255d45fe57fda27f651549f4ed71
rajatsvits <rajatmahajansvits@gmail.com>
2017-10-15 02:15:27 +0530

    Create AbsoluteValuesSumMinimization.java

---
+++ AbsoluteValuesSumMinimization.java
@@ -0,0 +1,38 @@
+package codeFight;
+
+public class AbsoluteValuesSumMinimization {
+
+	int absoluteValuesSumMinimization(int[] a) {
+
+		  int indexOfMinimum = -1;
+		  int minimalSum = Integer.MAX_VALUE;
+
+		  for (int i = 0; i < a.length; i++) {
+		    int sum = 0;
+		    for (int j = 0; j < a.length; j++) {
+		      sum +=  Math.abs(a[j]-a[i]) ;
+		    }
+		    if (sum < minimalSum) {
+		      minimalSum = sum;
+		      indexOfMinimum = i;
+		    }
+		  }
+
+		  return a[indexOfMinimum];
+		}
+
+}
+
+
+/*
+ * Given a sorted array of integers a, find an integer x from a such that the value of
+
+abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+is the smallest possible (here abs denotes the absolute value).
+If there are several possible answers, output the smallest one.
+
+Example
+
+For a = [2, 4, 7], the output should be
+absoluteValuesSumMinimization(a) = 4.
+ */
